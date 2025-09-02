3b89790bf9d09970cf5932ba99328b25273fb75d
Bertold Sedlak <bertold.sedlak@cyansecurity.com>
2025-09-02 12:38:42 +0200

    update autostart

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# Set wallpaper
+
+# Apply monitor layout FIRST
+xrandr \
+  --output DVI-I-1-1 --mode 5120x1440 --pos 1920x0 --primary \
+  --output DVI-I-2-2 --mode 2560x1440 --pos 7040x0 \
+  --output eDP-1     --mode 1920x1080 --pos 0x147
+
+# Wait for monitors to be configured
+sleep 1
+
+# Set wallpaper AFTER monitor configuration
 WALLPAPER=~/Pictures/vivek-kumar-JS_ohjocm00-unsplash.jpg
 xwallpaper --output DVI-I-1-1 --stretch "$WALLPAPER" \
            --output DVI-I-2-2 --stretch "$WALLPAPER" \
            --output eDP-1     --stretch "$WALLPAPER"
-# keyboard
+
+
+# Set keyboard layout
 setxkbmap us
 
 # Start compositor for transparency
@@ -13,11 +25,4 @@ picom &
 
 # Start flameshot in tray
 # flameshot &
-
-# Optional: custom xrandr setup for ultrawide
-# Apply monitor layout
-xrandr \
-  --output DVI-I-1-1 --mode 5120x1440 --pos 1920x0 --primary \
-  --output DVI-I-2-2 --mode 2560x1440 --pos 7040x0 \
-  --output eDP-1     --mode 1920x1080 --pos 0x147
 
